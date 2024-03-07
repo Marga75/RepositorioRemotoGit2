271fd409ebf691ca5c1c6bd8c0af2463adaf7180
Marga <Marga@LAPTOP-IS5113G3>
2024-03-07 22:18:51 +0100

    rama1

---
+++ Proyecto2/src/paqueteRama1/Rama1.java
@@ -0,0 +1,13 @@
+package paqueteRama1;
+
+public class Rama1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Mensaje 1");
+		
+		System.out.println("Mensaje 2");
+		
+	}
+
+}
