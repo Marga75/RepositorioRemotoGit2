2619a8fddd6a6a909636c1c36bb1c175bb987d0f
Marga <Marga@LAPTOP-IS5113G3>
2024-03-07 21:39:32 +0100

    Primera confirmación

---
+++ Proyecto2/src/paquete2/ClasePrincipal.java
@@ -0,0 +1,11 @@
+package paquete2;
+
+public class ClasePrincipal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Estamos haciendo el ejercicio 4.23 de la página 176.");
+
+	}
+
+}
